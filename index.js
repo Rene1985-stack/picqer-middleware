b7f62c50e7ff35c0639a558b921c4b63e622f8b3
Rene1985-stack <rene.denronde@skapaglobal.com>
2025-04-16 15:24:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const sql = require('mssql');
 
-// Updated configuration using SQL Authentication
+// Updated configuration using SQL Authentication with properly formatted server name
 const config = {
-    server: process.env.SQL_SERVER,
+    server: process.env.SQL_SERVER, // Should be in format: servername.database.windows.net
     database: process.env.SQL_DATABASE,
     user: process.env.SQL_USER,
     password: process.env.SQL_PASSWORD,
@@ -13,6 +13,7 @@ const config = {
 
 async function fetchData() {
     try {
+        console.log('Attempting to connect to SQL server:', process.env.SQL_SERVER);
         await sql.connect(config);
         const result = await sql.query`SELECT TOP 10 * FROM INFORMATION_SCHEMA.TABLES`;
         console.log(result.recordset);
